d139d57564c21fe7f0e2d875f6258645be866c02
bash <yesufubashir@gmail.com>
2023-11-15 22:06:26 +0100

    Add linter rules

---
+++ lib/constants/image_path.dart
@@ -0,0 +1,6 @@
+class ImagePath {
+  static const String _imageDir = "lib/assets/images";
+  static const String imageOne = "$_imageDir/image-1.svg";
+  static const String imageTwo = "$_imageDir/image-2.svg";
+  static const String imageThree = "$_imageDir/image-3.svg";
+}
